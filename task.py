a9f46cc9516773910e870ed52d9a2f3bcfaab10c
antonovivan-pi20-4 <70858685+antonovivan-pi20-4@users.noreply.github.com>
2021-11-22 05:17:35 +0300

    Поднятие веб-сервера Антонов ИМ ПИ20-4

---
+++ task.py
@@ -0,0 +1,18 @@
+import socket
+
+PORT = 80
+
+host = input('Введите имя хоста: ')
+resource = input('Введите идентификатор ресурса: ')
+if not resource:
+    resource = '/'
+elif resource[0] != '/':
+    resource = '/' + resource
+
+client = socket.socket()
+client.connect((host, PORT))
+request = f'GET {resource} HTTP/1.1\r\nHost:{host}\r\n\r\n'
+client.send(request.encode())
+
+reply = client.recv(8192).decode()
+print(reply)
